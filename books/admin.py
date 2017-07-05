34b5e3ec00c8526fcfab2a9fd00392028b29100a
dilipthakor <thakordili8@gmail.com>
2017-07-05 23:21:33 +0000

    add new author model many to many

--- books/admin.py
+++ books/admin.py
@@ -2,9 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Book
+from .models import Author, Book
 
 
 # Register your models here.
 admin.site.register(Book)
+admin.site.register(Author)
 
